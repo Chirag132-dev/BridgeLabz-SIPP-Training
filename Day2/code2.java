9fab81dafb64a48dc43be287b7ea013339b5d453
chirag <cshrivastava731@gmail.com>
2025-06-12 12:00:06 +0530

    second

---
+++ Day2/code2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class code2{
+    public static void main(String[] args) {
+        int r= 7378;
+        double c= 1.33;
+        double pi= 3.14;
+        
+
+    }
+}
