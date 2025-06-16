5f8d0a5d8e323887ff1145b6ef2e649cc76a3179
chirag <cshrivastava731@gmail.com>
2025-06-17 00:08:08 +0530

    [Chirag Shrivastava]Added some programs on Java Control Flow

---
+++ Control_Flows/FactorsFinder.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class FactorsFinder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        if(number > 0){
+            int counter = 1;
+            while(counter < number){
+                if(number % counter == 0){
+                    System.out.println(counter);
+                }
+                counter++;
+            }
+        } else {
+            System.out.println("Enter a positive integer.");
+        }
+    }
+}
